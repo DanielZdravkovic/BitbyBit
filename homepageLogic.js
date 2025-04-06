d99cd101f6f4000a08ced5234ddacbe3e2c63d48
Daniel Zdravkovic <daniel14012005@gmail.com>
2025-04-07 00:10:21 +0200

    Slogan + Fußzeile + Farbpalette geändert/ergänzt

--- homepageLogic.js
+++ homepageLogic.js
@@ -4,3 +4,6 @@ document.getElementById("product").addEventListener("click", function(){
 document.getElementById("back").addEventListener("click", function(){
     document.getElementById('produktPage').classList.remove('active');
 });
+document.getElementById("gmail").addEventListener("click", function(){
+    window.location.href = "mailto:bitbybit.mannheim@gmail.com";
+});

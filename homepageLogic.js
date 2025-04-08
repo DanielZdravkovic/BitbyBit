1e305dcbba8c17372b2c9a797c516bbbc11efc4a
Daniel Zdravkovic <daniel14012005@gmail.com>
2025-04-08 16:35:09 +0200

    Über uns Seite hinzugefügt

--- homepageLogic.js
+++ homepageLogic.js
@@ -1,8 +1,14 @@
 document.getElementById("product").addEventListener("click", function(){
     document.getElementById("produktPage").classList.add("active");
 });
-document.getElementById("back").addEventListener("click", function(){
+document.getElementById("rightback").addEventListener("click", function(){
     document.getElementById('produktPage').classList.remove('active');
+});
+document.getElementById("aboutus").addEventListener("click", function(){
+    document.getElementById("aboutusPage").classList.add("active");
+});
+document.getElementById("leftback").addEventListener("click", function(){
+    document.getElementById('aboutusPage').classList.remove('active');
 });
 document.getElementById("gmail").addEventListener("click", function(){
     window.location.href = "mailto:bitbybit.mannheim@gmail.com";
